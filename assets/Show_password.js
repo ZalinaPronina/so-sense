ee1fa7135fd1cb40938281bb1f71ac5394a27e31
Kochun1994 <vaniakochin94@gmail.com>
2022-10-07 18:54:30 +0300

    nev changes login pop up

--- assets/Show_password.js
+++ assets/Show_password.js
@@ -3,10 +3,28 @@ function ShowPassword() {
     document.getElementById("close-password").style.display = "block";
     document.getElementById("hide-password").style.display = "none";
         x.type = "text";
+
 }
 function HidePassword() {
     var x = document.getElementById("CustomerPassword");
+    var y = document.getElementById("RegisterForm-password");
     document.getElementById("close-password").style.display = "none";
     document.getElementById("hide-password").style.display = "block";
         x.type = "password";
+        y.type = "password";
+}
+function ShowPasswordRegister() {
+    var y = document.getElementById("RegisterForm-password");
+    y.type = "text";
+    document.getElementById("close-password").style.display = "block";
+    document.getElementById("hide-password").style.display = "none";
+}
+function HidePasswordRegister() {
+    var y = document.getElementById("RegisterForm-password");
+    document.getElementById("close-password").style.display = "none";
+    document.getElementById("hide-password").style.display = "block";
+    y.type = "password";
+}
+function ClosedPoUp() {
+    document.getElementById("pop_up").style.display = "none";
 }
